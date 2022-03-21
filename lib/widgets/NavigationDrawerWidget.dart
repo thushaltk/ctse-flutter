33f0db021303471449a4b43d8e04790bfc8e5fab
Thushal Tharaka Kulatilake <thushal1999@gmail.com>
2022-03-22 03:19:48 +0530

    updated

--- lib/widgets/NavigationDrawerWidget.dart
+++ lib/widgets/NavigationDrawerWidget.dart
@@ -8,55 +8,94 @@ class NavigationDrawyerWidget extends StatelessWidget {
     return Drawer(
       child: Container(
         alignment: Alignment.topCenter,
-        child: Stack(
-          children: [
-            SizedBox(
-                height: 230,
-                width: double.infinity,
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 1, 77, 123),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Tap.",
-                          style: TextStyle(
-                              fontFamily: "Tenorite",
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 50),
-                          textAlign: TextAlign.start,
-                        ),
-                        Text(
-                          "Explore.",
-                          style: TextStyle(
-                              fontFamily: "Tenorite",
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 50),
-                          textAlign: TextAlign.start,
-                        ),
-                        Text(
-                          "Learn.",
-                          style: TextStyle(
-                              fontFamily: "Tenorite",
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 50),
-                          textAlign: TextAlign.start,
-                        )
-                      ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                  height: 230,
+                  width: double.infinity,
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 1, 77, 123),
                     ),
-                  ),
-                )),
-          ],
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18.0, 0, 0, 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Tap.",
+                            style: TextStyle(
+                                fontFamily: "Tenorite",
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50),
+                            textAlign: TextAlign.start,
+                          ),
+                          Text(
+                            "Explore.",
+                            style: TextStyle(
+                                fontFamily: "Tenorite",
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50),
+                            textAlign: TextAlign.start,
+                          ),
+                          Text(
+                            "Learn.",
+                            style: TextStyle(
+                                fontFamily: "Tenorite",
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50),
+                            textAlign: TextAlign.start,
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+              SizedBox(
+                height: 280,
+                child: ListView(
+                  children: <Widget>[
+                    buildMenuItem(
+                      text: 'Home',
+                      icon: Icons.home_filled
+                    ),
+                    buildMenuItem(
+                      text: 'Help',
+                      icon: Icons.help
+                    ),
+                    buildMenuItem(
+                      text: 'Share',
+                      icon: Icons.share
+                    ),
+                    buildMenuItem(
+                      text: 'Admin',
+                      icon: Icons.admin_panel_settings
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget buildMenuItem({
+    required String text,
+    required IconData icon
+  }) {
+    final color = Color.fromARGB(255, 1, 77, 123);
+
+    return ListTile(
+      leading: Icon(icon, color: color,),
+      title: Text(text, style: TextStyle(color: color, fontSize: 20, fontFamily: "Tenorite")),
+      onTap: () {},
     );
   }
 }
